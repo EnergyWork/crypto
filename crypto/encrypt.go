4bbab842f65b2353873ab15b85e83954130d409f
Artyom <ag.energy@mail.ru>
2021-06-10 17:44:46 +0300

    неверно работающий encrypt

--- crypto/encrypt.go
+++ crypto/encrypt.go
@@ -1,7 +1,6 @@
 package crypto
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -9,7 +8,7 @@ import (
 func Convolution(arr []string) []string {
 	var seq []string
 	for i := 0; i < len(arr); i++ {
-		var counter int = 0
+		var counter = 0
 		for j := i; j < len(arr) && arr[j] == arr[i]; j++ {
 			counter += 1
 		}
@@ -24,6 +23,20 @@ func Convolution(arr []string) []string {
 
 func Encrypt(arr []string) ([]string, error) {
 	seq := Convolution(arr)
-	fmt.Println(strings.Join(seq, ""))
+	var result []string
+	for step := 1; step <= (len(seq) / 2); step++ {
+		var counter = 0
+		seq1 := strings.Join(seq[0:0+step], "")
+		for j := 0; j < len(seq) - step; j += step {
+			seq2 := strings.Join(seq[j+step:j+2*step], "")
+			if seq1 == seq2 {
+				counter += 1
+			}
+		}
+		if counter != 1 {
+			result = append(result, strconv.Itoa(counter))
+		}
+		result = append(result, seq1)
+	}
 	return seq, nil
 }
