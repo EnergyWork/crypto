a8d0f791b5d8bb88408085a5b3ab30bd014a7dfb
Artyom <ag.energy@mail.ru>
2021-06-10 15:04:02 +0300

    исправлены пути до функции для теста

--- decrypt_test.go
+++ decrypt_test.go
@@ -1,49 +1,51 @@
 package main
 
 import (
+	"api-crypto/crypto"
 	"strings"
 	"testing"
 )
 
 func TestDecrypt(t *testing.T) {
-	decoded := Decrypt(strings.Split("a2(c2(ab)f)d2c", ""))
+	t.Log("1")
+	decoded, _ := crypto.Decrypt(strings.Split("a2(c2(ab)f)d2c", ""))
 	sdecoded := strings.Join(decoded, "")
 	decoded_real := "acababfcababfdcc"
 	if sdecoded != decoded_real {
 		t.Errorf("%s not equal %s", sdecoded, decoded_real)
 	}
-
-	decoded = Decrypt(strings.Split("2a(c2e)", ""))
+	t.Log("2")
+	decoded, _ = crypto.Decrypt(strings.Split("2a(c2e)", ""))
 	sdecoded = strings.Join(decoded, "")
 	decoded_real = "aacee"
 	if sdecoded != decoded_real {
 		t.Errorf("%s not equal %s", sdecoded, decoded_real)
 	}
-
-	decoded = Decrypt(strings.Split("2a", ""))
+	t.Log("3")
+	decoded, _ = crypto.Decrypt(strings.Split("2a", ""))
 	sdecoded = strings.Join(decoded, "")
 	decoded_real = "aa"
 	if sdecoded != decoded_real {
 		t.Errorf("%s not equal %s", sdecoded, decoded_real)
 	}
-
-	decoded = Decrypt(strings.Split("a2(er)2(vf)2r", ""))
+	t.Log("4")
+	decoded, _ = crypto.Decrypt(strings.Split("a2(er)2(vf)2r", ""))
 	sdecoded = strings.Join(decoded, "")
 	decoded_real = "aerervfvfrr"
 	if sdecoded != decoded_real {
 		t.Errorf("%s not equal %s", sdecoded, decoded_real)
 	}
-
-	decoded = Decrypt(strings.Split("2(ar)2(e2(ho))", ""))
+	t.Log("5")
+	decoded, _ = crypto.Decrypt(strings.Split("2(ar)2(e2(ho))", ""))
 	sdecoded = strings.Join(decoded, "")
 	decoded_real = "ararehohoehoho"
 	if sdecoded != decoded_real {
 		t.Errorf("%s not equal %s", sdecoded, decoded_real)
 	}
-
-	decoded = Decrypt(strings.Split("(abc2d)", ""))
+	t.Log("6")
+	decoded, _ = crypto.Decrypt(strings.Split("(2a)", ""))
 	sdecoded = strings.Join(decoded, "")
-	decoded_real = "aabcdd"
+	decoded_real = "aa"
 	if sdecoded != decoded_real {
 		t.Errorf("%s not equal %s", sdecoded, decoded_real)
 	}
